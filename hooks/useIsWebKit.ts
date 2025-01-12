a2dbb30ac8ad556040c44221251d07f354875fe3
dwiki.arlan <arrlancore@gmail.com>
2025-01-12 07:14:21 +0700

    fix: is web kit

--- hooks/useIsWebKit.ts
+++ hooks/useIsWebKit.ts
@@ -4,12 +4,20 @@ export const useIsWebKit = (): boolean => {
   const [isWebKit, setIsWebKit] = useState(true);
 
   useEffect(() => {
-    const checkIsWebKit = () => {
-      const userAgent = navigator.userAgent;
-      return /WebKit/.test(userAgent) && !/Edge/.test(userAgent);
+    const checkIsWebKitOrSafari = () => {
+      const userAgent = navigator.userAgent.toLowerCase();
+      const isWebKit = /webkit/.test(userAgent);
+      const isNotEdge = !/edge/.test(userAgent);
+      const isSafari = /safari/.test(userAgent);
+      const isChrome = /chrome/.test(userAgent);
+      const isIOS =
+        /ipad|iphone|ipod/.test(userAgent) && !(window as any).MSStream;
+
+      // Safari on iOS doesn't include "Safari" in UA string in standalone mode
+      return isWebKit && isNotEdge && (isSafari || isChrome || isIOS);
     };
 
-    setIsWebKit(checkIsWebKit());
+    setIsWebKit(checkIsWebKitOrSafari());
   }, []);
 
   return isWebKit;
