b40c095c20bf06137a5aff32de87c7578dfa9d3b
dwiki.arlan <arrlancore@gmail.com>
2026-02-20 10:33:03 +0700

    fix: deps

--- hooks/common.tsx
+++ hooks/common.tsx
@@ -1,9 +1,15 @@
-import { convertMDToContent } from "@/lib/mdx/mdx-utils";
+import { MDXRemote } from "next-mdx-remote/rsc";
 
-export const renderMd = (content: string) =>
-  convertMDToContent(content, {
-    p: (props: any) => <p style={{ fontSize: "1rem" }} {...props} />,
-    h1: (props: any) => <h1 style={{ fontSize: "2rem" }} {...props} />,
-    h2: (props: any) => <h2 style={{ fontSize: "1.5rem" }} {...props} />,
-    h3: (props: any) => <h3 style={{ fontSize: "1.25rem" }} {...props} />,
-  });
+export const renderMd = async (content: string) => {
+  return (
+    <MDXRemote
+      source={content}
+      components={{
+        p: (props: any) => <p style={{ fontSize: "1rem" }} {...props} />,
+        h1: (props: any) => <h1 style={{ fontSize: "2rem" }} {...props} />,
+        h2: (props: any) => <h2 style={{ fontSize: "1.5rem" }} {...props} />,
+        h3: (props: any) => <h3 style={{ fontSize: "1.25rem" }} {...props} />,
+      }}
+    />
+  );
+};
--- lib/author-utils.ts
+++ lib/author-utils.ts
@@ -2,8 +2,6 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import { Author } from "@/types/blog";
-import { MDXRemote } from "next-mdx-remote/rsc";
-import components from "./mdx/mdx-components";
 
 const authorsDirectory = path.join(process.cwd(), "content/authors");
 
@@ -17,15 +15,10 @@ export async function getAuthorBySlug(slug: string): Promise<Author | null> {
   const fileContents = fs.readFileSync(fullPath, "utf8");
   const { data, content } = matter(fileContents);
 
-  const mdxContent = await MDXRemote({
-    source: content,
-    components: components,
-  });
-
   const author: Author = {
     name: data.name,
     slug: slug,
-    bio: mdxContent,
+    bio: content, // Store MDX source string
     avatar: data.avatar,
     socialLinks: data.socialLinks,
     summary: data.summary,
